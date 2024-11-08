d6b8b21dd2d5370b1269941056ae2e9fe6fe40f9
Annie <161513339+ANZIE05@users.noreply.github.com>
2024-11-09 00:25:02 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
           <Image src={pp} alt="profile picture" width={360} height= {350}/> </div>
 
         <div className="home-content">
-          <h1>Hi, I'm Qurat Ul Ain Ismail</h1>
+          <h1>Hi, I am Qurat Ul Ain Ismail</h1>
           <h3>Frontend Developer</h3>
           <h3>Graphic Designer</h3>
           <h3>Digital Marketer</h3>
           <h3>MS Office</h3>
-          <p>Certified Graphic Designer and Digital Marketer with expertise in MS Office, I bring a creative edge to every project. Currently enhancing my front-end development skills, I'm passionate about crafting visually compelling and user-friendly designs. Click "Read More" to learn about my journey, skills, and commitment to impactful work.</p>
+          <p>Certified Graphic Designer and Digital Marketer with expertise in MS Office, I bring a creative edge to every project. Currently enhancing my front-end development skills, I am passionate about crafting visually compelling and user-friendly designs. Click &quot;Read More&quot; to learn about my journey, skills, and commitment to impactful work.</p>
 
           <div className="home-sci">
             
