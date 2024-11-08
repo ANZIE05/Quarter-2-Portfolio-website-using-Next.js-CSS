ce2a028b15080564d145e3442395e8ce28758462
Annie <161513339+ANZIE05@users.noreply.github.com>
2024-11-09 00:31:57 +0500

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function ProjectPage (){
                 <Image src="/sc.jpg" alt="simple calculator" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Simple Calculator</h4>
-                    <p>"Built a simple calculator using TypeScript for basic arithmetic operations."</p>
+                    <p>Built a simple calculator using TypeScript for basic arithmetic operations.</p>
                     <a href="https://github.com/ANZIE05/Simple-Calculator-Assignment-"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -22,7 +22,7 @@ export default function ProjectPage (){
                 <Image src="/atm.jpg" alt="ATM" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>ATM</h4>
-                    <p>"ATM simulation built with TypeScript, enabling secure transactions and operations."</p>
+                    <p>ATM simulation built with TypeScript, enabling secure transactions and operations.</p>
                     <a href="https://github.com/ANZIE05/ATM-Machine-"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -31,7 +31,7 @@ export default function ProjectPage (){
                 <Image src="/ct.jpeg" alt="countdown timer" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Countdown Timer</h4>
-                    <p>"Interactive countdown timer built with TypeScript, featuring precise time tracking."</p>
+                    <p>Interactive countdown timer built with TypeScript, featuring precise time tracking.</p>
                     <a href="https://github.com/ANZIE05/Countdown-Timer"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -40,7 +40,7 @@ export default function ProjectPage (){
                 <Image src="/cc.png" alt="currency converter" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Currency Converter</h4>
-                    <p>"Currency Converter built with TypeScript for quick, accurate rate conversions."</p>
+                    <p>Currency Converter built with TypeScript for quick, accurate rate conversions.</p>
                     <a href="https://github.com/ANZIE05/Currency-Converter"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -49,7 +49,7 @@ export default function ProjectPage (){
                 <Image src="/qg.jpeg" alt="quiz system" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Quiz System</h4>
-                    <p>"Interactive quiz system built with TypeScript for seamless learning experiences."</p>
+                    <p>Interactive quiz system built with TypeScript for seamless learning experiences.</p>
                     <a href="https://github.com/ANZIE05/Quiz-System"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -58,7 +58,7 @@ export default function ProjectPage (){
                 <Image src="/gng.jpg" alt="guessing number game" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Guessing Number Game</h4>
-                    <p>"Interactive Guessing Number Game: Built with TypeScript for fun learning!"</p>
+                    <p>Interactive Guessing Number Game: Built with TypeScript for fun learning!</p>
                     <a href="https://github.com/ANZIE05/Number-Guessing-Game"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -67,7 +67,7 @@ export default function ProjectPage (){
                 <Image src="/sms.png" alt="student management system" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Student Management System</h4>
-                    <p>"Student Management System built with TypeScript for efficient data handling."</p>
+                    <p>Student Management System built with TypeScript for efficient data handling.</p>
                     <a href="https://github.com/ANZIE05/Student-Management-System"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -76,7 +76,7 @@ export default function ProjectPage (){
                 <Image src="/ag.jpeg" alt="adventure game" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Adventure Game</h4>
-                    <p>"Adventure game built with TypeScript, offering interactive, immersive gameplay experience."</p>
+                    <p>Adventure game built with TypeScript, offering interactive, immersive gameplay experience.</p>
                     <a href="https://github.com/ANZIE05/Adventure-Game"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -85,7 +85,7 @@ export default function ProjectPage (){
                 <Image src="/tdl.jpeg" alt="to-do list" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>To-do List</h4>
-                    <p>"Interactive To-Do List built with TypeScript for efficient task management."</p>
+                    <p>Interactive To-Do List built with TypeScript for efficient task management.</p>
                     <a href="https://github.com/ANZIE05/To_do-list"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -94,7 +94,7 @@ export default function ProjectPage (){
                 <Image src="/wc.jpg" alt="word counter" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>Word Counter</h4>
-                    <p>"Word counter tool built with TypeScript for efficient text analysis."</p>
+                    <p>Word counter tool built with TypeScript for efficient text analysis.</p>
                     <a href="https://github.com/ANZIE05/Word-Counter"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -103,7 +103,7 @@ export default function ProjectPage (){
                 <Image src="/oop.jpeg" alt="oop" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>OOP</h4>
-                    <p>"Object-Oriented Programming in TypeScript: Structured, reusable code with classes."</p>
+                    <p>Object-Oriented Programming in TypeScript: Structured, reusable code with classes.</p>
                     <a href="https://github.com/ANZIE05/OOP"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
@@ -112,7 +112,7 @@ export default function ProjectPage (){
                 <Image src="/bank1.jpeg" alt="oop my bank" width={600} height={450}/>
                 <div className="portfolio-layer">
                     <h4>OOP My Bank</h4>
-                    <p>"OOP-based banking system in TypeScript for secure, efficient transactions."</p>
+                    <p>OOP-based banking system in TypeScript for secure, efficient transactions.</p>
                     <a href="https://github.com/ANZIE05/OOP-My-Bank"  target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
                 </div>
             </div>
